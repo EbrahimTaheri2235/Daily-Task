dacc54668aa5e8e895acf0d8ba441f6e77c079da
EbrahimTaheri2235 <javad1385taheri@gmail.com>
2023-09-01 19:31:21 +0330

    Task

---
+++ ReadMe.md
@@ -0,0 +1,19 @@
+# Daily Task Display Project
+
+This project is a simple web application that allows you to input a daily task title and description. It then displays the task on the page when you click the "Display" button.
+
+## Getting Started
+
+To get started with this project, follow these steps:
+
+1. Clone the repository to your local machine: `https://github.com/EbrahimTaheri2235/Daily-Task.git`
+
+2. Open the `index.html` file in your web browser.
+
+## Usage
+
+1. Enter the title and description of your daily task in the input fields provided.
+
+2. Click the "Display" button to show your task on the page.
+
+
---
+++ main.ts
@@ -0,0 +1,19 @@
+// Define types for your data (optional)
+type Task = {
+    title: string;
+    description: string;
+};
+
+function displayTask() {
+    const titleElement = document.getElementById("title") as HTMLInputElement;
+    const descriptionElement = document.getElementById("description") as HTMLTextAreaElement;
+
+    const title = titleElement.value;
+    const description = descriptionElement.value;
+
+    const taskOutput = document.getElementById("taskOutput");
+    taskOutput!.innerHTML = `<h2 class="text-xl font-semibold">Title: ${title}</h2><p class="text-gray-700">Description: ${description}</p>`;
+}
+
+const showTaskButton = document.getElementById("showTaskButton");
+showTaskButton!.addEventListener("click", displayTask);
